2c652063f5d5048c4ba9a6f60185f941bc9043ca
Sandeep Jagtap <sandeep.jagtap@gmail.com>
2012-01-23 19:35:20 -0600

    empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendsGraph::Application.config.secret_token = 'f819e1d13fb7e68c2ed5c408ab1ee80766179b0cf321fa3c0c9930aa22a10cec2ff3b4d369d9da6b7ec5eba2a688776bdfc3eebad92feba94ced008e565b5b65'
